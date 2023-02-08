dbfe207986ee8aabd30db2382ba16d0b046f0dc5
Katie Lee <klee1215@hotmail.com>
2023-02-08 10:40:28 -0800

    Copy pasta from 02/06 class)

--- src/gullienne.js
+++ src/gullienne.js
@@ -5,3 +5,34 @@ console.log("They waterboarded the coconut")
 export function add(x, y) {
   return x + y
 }
+
+/*#! /usr/bin/env node
+
+import fs from "fs/promises"
+import process from "process"
+import compile from "./compiler.js"
+
+const help = `Gullienne compiler
+Syntax: node gullienne.js <filename> <outputType>
+Prints to stdout according to <outputType>, which must be one of:
+  analyzed   the semantically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+`
+
+async function compileFromFile(filename, outputType) {
+  try {
+    const buffer = await fs.readFile(filename)
+    console.log(compile(buffer.toString(), outputType))
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`)
+    process.exitCode = 1
+  }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
+}
+*/
