21daa30c1494b673fd7c14d032e30da956de01a2
Alex Abrams <alexabrams240@gmail.com>
2023-03-15 18:55:54 -0700

    Revert "fixed tests again"
    This reverts commit f15e0f4f3de1a7c38580b6fb9c9ce0e11a184719.

--- src/ast.js
+++ src/ast.js
@@ -11,63 +11,63 @@ const gullienneGrammar = ohm.grammar(fs.readFileSync("src/gullienne.ohm"))
 
 const astBuilder =gullienneGrammar.createSemantics().addOperation("ast", {
   Program(body) {
-    return new core.Program(body.ast())
+    return new core.Program(body.rep())
   },
   Statement(statement) {
-    return statement.ast()
+    return statement.rep()
   },
   Statement_standalone(expression, _semi) {
-    return new core.Statement(expression.ast())
+    return new core.Statement(expression.rep())
   },
   Statement_incDec(id, operator, _semi) {
-    return new core.IncDecStatement(id.ast(), operator.sourceString)
+    return new core.IncDecStatement(id.rep(), operator.sourceString)
   },
   VarDec(id, _colon, type, _at, initializer, _semi) {
     return new core.VariableDeclaration(
-      id.ast(),
-      type.ast(),
-      initializer.ast()
+      id.rep(),
+      type.rep(),
+      initializer.rep()
     )
   },
   Reassign(id, _at, source, _semi) {
-    return new core.ReassignmentStatement(id.ast(), source.ast())
+    return new core.ReassignmentStatement(id.rep(), source.rep())
   },
   ReassignMy(_my, _dot, id, _at, source, _semi) {
-    return new core.ReassignmentMyStatement(id.ast(), source.ast())
+    return new core.ReassignmentMyStatement(id.rep(), source.rep())
   },
   FunctDec(_do, id, _lp, params, _rp, _arrow, returnType, funcBlock) {
     return new core.FunctionDeclaration(
-      id.ast(),
-      params.asIteration().ast(),
-      returnType.ast(),
-      funcBlock.ast()
+      id.rep(),
+      params.asIteration().rep(),
+      returnType.rep(),
+      funcBlock.rep()
     )
   },
   FuncBlock(_lc, base, statements, _rc) {
-    return new core.FunctionBlock(base.ast(), statements.ast())
+    return new core.FunctionBlock(base.rep(), statements.rep())
   },
   If(_so, _lp, testExp, _rp, genBlock, listOfButs, otherwise) {
     return new core.ConditionIf(
-      testExp.ast(),
-      genBlock.ast(),
-      listOfButs.ast(),
-      otherwise.ast()
+      testExp.rep(),
+      genBlock.rep(),
+      listOfButs.rep(),
+      otherwise.rep()
     )
   },
   ElseIf(_but, _lp, testExp, _rp, genBlock) {
-    return new core.ConditionElseIf(testExp.ast(), genBlock.ast())
+    return new core.ConditionElseIf(testExp.rep(), genBlock.rep())
   },
   Else(_otherwise, genBlock) {
-    return new core.ConditionElse(genBlock.ast())
+    return new core.ConditionElse(genBlock.rep())
   },
   ForLoop(_cap, _lp, id, _in, expression, _rp, genBlock) {
-    return new core.ForLoop(id.ast(), expression.ast(), genBlock.ast())
+    return new core.ForLoop(id.rep(), expression.rep(), genBlock.rep())
   },
   WhileLoop(_noCap, _lp, expression, _rp, genBlock) {
-    return new core.WhileLoop(expression.ast(), genBlock.ast())
+    return new core.WhileLoop(expression.rep(), genBlock.rep())
   },
   Return(_howItBe, expression, _semi) {
-    return new core.ReturnStatement(expression.ast())
+    return new core.ReturnStatement(expression.rep())
   },
   Break(_frogOut, _semi) {
     return new core.BreakStatement()
@@ -77,147 +77,147 @@ const astBuilder =gullienneGrammar.createSemantics().addOperation("ast", {
   },
   ObjectHead(_object, id, _lp, params, _rp, objectBlock) {
     return new core.ObjectHeader(
-      id.ast(),
-      params.asIteration().children.map((child) => child.ast()),
-      objectBlock.ast()
+      id.rep(),
+      params.asIteration().children.map((child) => child.rep()),
+      objectBlock.rep()
     )
   },
   ConstrucDec(_const, _lp, params, _rp, genBlock) {
     return new core.ConstructDeclaration(
-      params.asIteration().ast(),
-      genBlock.ast()
+      params.asIteration().rep(),
+      genBlock.rep()
     )
   },
   ObjectBlock(_lc, base, construcDec, methodDec, _rc) {
     return new core.ObjectBlock(
-      base.ast(),
-      construcDec.ast(),
-      methodDec.ast()
+      base.rep(),
+      construcDec.rep(),
+      methodDec.rep()
     )
   },
   MethodDec(_do, _hash, id, _lp, params, _rp, _arrow, returnType, funcBlock) {
     return new core.MethodDeclaration(
-      id.ast(),
-      params.asIteration().ast(),
-      returnType.ast(),
-      funcBlock.ast()
+      id.rep(),
+      params.asIteration().rep(),
+      returnType.rep(),
+      funcBlock.rep()
     )
   },
   Base(_bayes, id, _at, expression, _semi) {
-    return new core.Base(id.ast(), expression.ast())
+    return new core.Base(id.rep(), expression.rep())
   },
   GenBlock(_lc, statements, _rc) {
-    return new core.GeneralBlock(statements.ast())
+    return new core.GeneralBlock(statements.rep())
   },
   RealParam(id, _colon, type) {
-    return new core.RealParameter(id.ast(), type.ast())
+    return new core.RealParameter(id.rep(), type.rep())
   },
   DecParam(params) {
     if (params.sourceString == "/") {
       return "/"
     } else {
-      return new core.DeclarationParameter(params.ast())
+      return new core.DeclarationParameter(params.rep())
     }
   },
   CallArg_kwarg(id, _colon, expression) {
-    return new core.CallArgument(id.ast(), expression.ast())
+    return new core.CallArgument(id.rep(), expression.rep())
   },
   CallArg_parg(expression) {
-    return new core.CallArgument(null, expression.ast())
+    return new core.CallArgument(null, expression.rep())
   },
   ListLit(_lbrac, expression, _rbrac) {
-    return new core.ListLiteral(expression.asIteration().ast())
+    return new core.ListLiteral(expression.asIteration().rep())
   },
   SetLit(_lab, expression, _rab) {
-    return new core.SetLiteral(expression.asIteration().ast())
+    return new core.SetLiteral(expression.asIteration().rep())
   },
   MapLit(_ldab, keyValue, _rdab) {
-    return new core.MapLiteral(keyValue.asIteration().ast())
+    return new core.MapLiteral(keyValue.asIteration().rep())
   },
   KeyValue(expression1, _dcolon, expression2) {
-    return new core.KeyValue(expression1.ast(), expression2.ast())
+    return new core.KeyValue(expression1.rep(), expression2.rep())
   },
   ExpFull_joolOr(left, _op, right) {
-    return new core.BinaryExpression(left.ast(), "|", right.ast())
+    return new core.BinaryExpression(left.rep(), "|", right.rep())
   },
   Exp8_joolAnd(left, _op, right) {
-    return new core.BinaryExpression(left.ast(), "&", right.ast())
+    return new core.BinaryExpression(left.rep(), "&", right.rep())
   },
   Exp7_eqNeq(left, op, right) {
     if (op.sourceString == "=") {
-      return new core.BinaryExpression(left.ast(), "=", right.ast())
+      return new core.BinaryExpression(left.rep(), "=", right.rep())
     } else {
-      return new core.BinaryExpression(left.ast(), "!=", right.ast())
+      return new core.BinaryExpression(left.rep(), "!=", right.rep())
     }
   },
   Exp6_glte(left, op, right) {
     if (op.sourceString == "<=") {
-      return new core.BinaryExpression(left.ast(), "<=", right.ast())
+      return new core.BinaryExpression(left.rep(), "<=", right.rep())
     } else if (op.sourceString == ">=") {
-      return new core.BinaryExpression(left.ast(), ">=", right.ast())
+      return new core.BinaryExpression(left.rep(), ">=", right.rep())
     } else if (op.sourceString == "<") {
-      return new core.BinaryExpression(left.ast(), "<", right.ast())
+      return new core.BinaryExpression(left.rep(), "<", right.rep())
     } else {
-      return new core.BinaryExpression(left.ast(), ">", right.ast())
+      return new core.BinaryExpression(left.rep(), ">", right.rep())
     }
   },
   Exp5_addDubUnionInter(left, op, right) {
     if (op.sourceString == "+") {
-      return new core.BinaryExpression(left.ast(), "+", right.ast())
+      return new core.BinaryExpression(left.rep(), "+", right.rep())
     } else if (op.sourceString == "-") {
-      return new core.BinaryExpression(left.ast(), "-", right.ast())
+      return new core.BinaryExpression(left.rep(), "-", right.rep())
     } else if (op.sourceString == "union") {
-      return new core.BinaryExpression(left.ast(), "union", right.ast())
+      return new core.BinaryExpression(left.rep(), "union", right.rep())
     } else {
-      return new core.BinaryExpression(left.ast(), "intersect", right.ast())
+      return new core.BinaryExpression(left.rep(), "intersect", right.rep())
     }
   },
   Exp4_mulDivRem(left, op, right) {
     if (op.sourceString == "*") {
-      return new core.BinaryExpression(left.ast(), "*", right.ast())
+      return new core.BinaryExpression(left.rep(), "*", right.rep())
     } else if (op.sourceString == "/") {
-      return new core.BinaryExpression(left.ast(), "/", right.ast())
+      return new core.BinaryExpression(left.rep(), "/", right.rep())
     } else {
-      return new core.BinaryExpression(left.ast(), "%", right.ast())
+      return new core.BinaryExpression(left.rep(), "%", right.rep())
     }
   },
   Exp3_expo(left, _op, right) {
-    return new core.BinaryExpression(left.ast(), "^", right.ast())
+    return new core.BinaryExpression(left.rep(), "^", right.rep())
   },
   Exp3_neg(op, right) {
     if (op.sourceString == "-") {
-      return new core.UnaryExpression("-", right.ast())
+      return new core.UnaryExpression("-", right.rep())
     } else {
-      return new core.UnaryExpression("!", right.ast())
+      return new core.UnaryExpression("!", right.rep())
     }
   },
   Exp2_listInd(subscriptee, _lbrac, argument, _rbrac) {
-    return new core.SubscriptExpression(subscriptee.ast(), argument.ast())
+    return new core.SubscriptExpression(subscriptee.rep(), argument.rep())
   },
   Exp2_mapInd(subscriptee, _ldab, argument, _rdab) {
-    return new core.SubscriptExpression(subscriptee.ast(), argument.ast())
+    return new core.SubscriptExpression(subscriptee.rep(), argument.rep())
   },
   Exp2_setInd(subscriptee, _lab, argument, _rab) {
-    return new core.SubscriptExpression(subscriptee.ast(), argument.ast())
+    return new core.SubscriptExpression(subscriptee.rep(), argument.rep())
   },
   Exp2_functCall(expression, _lp, argument, _rp) {
-    return new core.Call(expression.ast(), argument.asIteration().ast())
+    return new core.Call(expression.rep(), argument.asIteration().rep())
   },
   Exp2_objField(expression, _dot, id) {
-    return new core.FieldExpression(expression.ast(), id.ast())
+    return new core.FieldExpression(expression.rep(), id.rep())
   },
   Exp2_objMethod(expression, _dot, id, _lp, argument, _rp) {
     return new core.MethodExpression(
-      expression.ast(),
-      id.ast(),
-      argument.asIteration().ast()
+      expression.rep(),
+      id.rep(),
+      argument.asIteration().rep()
     )
   },
   Exp2_objMake(_make, type, _lp, argument, _rp) {
-    return new core.MakeExpression(type.ast(), argument.asIteration().ast())
+    return new core.MakeExpression(type.rep(), argument.asIteration().rep())
   },
   Exp1_expr(_lp, expression, _rp) {
-    return new core.Expression(expression.ast())
+    return new core.Expression(expression.rep())
   },
   id(letter, rest) {
     return this.sourceString
@@ -232,19 +232,19 @@ const astBuilder =gullienneGrammar.createSemantics().addOperation("ast", {
     return true
   },
   Type(type) {
-    return new core.Type(type.ast())
+    return new core.Type(type.rep())
   },
   Type_sumType(type1, _or, type2) {
-    return new core.TypeSum(type1.ast(), type2.ast())
+    return new core.TypeSum(type1.rep(), type2.rep())
   },
   Type_listType(_lb, type, _rb) {
-    return new core.TypeList(type.ast())
+    return new core.TypeList(type.rep())
   },
   Type_setType(_lab, type, _rab) {
-    return new core.TypeSet(type.ast())
+    return new core.TypeSet(type.rep())
   },
   Type_mapType(_ldab, keyType, _dc, valueType, _rdab) {
-    return new core.TypeMap(keyType.ast(), valueType.ast())
+    return new core.TypeMap(keyType.rep(), valueType.rep())
   },
   Type_custom(letter, rest) {
     return this.sourceString
@@ -253,7 +253,7 @@ const astBuilder =gullienneGrammar.createSemantics().addOperation("ast", {
     return this.sourceString
   },
   _iter(...children) {
-    return children.map((child) => child.ast())
+    return children.map((child) => child.rep())
   },
 })
 
@@ -262,5 +262,5 @@ export default function ast(sourceCode) {
   if (!match.succeeded()) {
     throw new error(match.message)
   }
-  return astBuilder(match).ast()
+  return analyzer(match).rep()
 }
